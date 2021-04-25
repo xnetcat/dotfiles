982943660cd973ffa55104482a48677af29db851
Jakub Kot <42355410+xnetcat@users.noreply.github.com>
2021-04-25 22:40:05 +0200

    wallpaper & improvements

--- install.sh
+++ install.sh
@@ -12,16 +12,20 @@ mkdir ~/.backup_dotfiles
 # Create backup 
 mv -rf ~/.config ~/.backup_dotfiles/config
 mv -rf ~/.local/share/fonts ~/.backup_dotfiles/fonts
+mv -rf ~/.wallpapers ~/.backup_dotfiles/wallpapers
 
 # Copy config files
 export XDG_CONFIG_HOME=$HOME/.config
 echo "export XDG_CONFIG_HOME=\$HOME/.config" >> ~/.bashrc
-cp -R ~/.dotfiles/config ~/.config 
+cp -r ~/.dotfiles/config ~/.config 
 
 # Set up fonts
 mkdir -p ~/.local/share/fonts
-cp -R ~/.dotfiles/fonts ~/.local/share/fonts 
+cp -r ~/.dotfiles/fonts ~/.local/share/fonts 
 sudo fc-cache -v -f
+
+# Copy wallpapers
+cp -r ~/.dotfiles/wallpapers ~/.wallpapers
 
 # Start dbus
 dbus-launch dconf load / < ~/.dotfiles/home/xed.dconf
