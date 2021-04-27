8f586e583d4ca3bcc8b749ee53a2fa91f84bcd1a
Jakub Kot <42355410+xnetcat@users.noreply.github.com>
2021-04-27 23:47:43 +0200

    copy config before bd install

--- install.sh
+++ install.sh
@@ -63,19 +63,19 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 echo "[INFO] Installing nvm"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
+# Copy config files
+echo "[INFO] Copy config files"
+export XDG_CONFIG_HOME=$HOME/.config
+cp -r ~/.dotfiles/config ~/.config 2> /dev/null
+cp ~/.dotfiles/home/.* ~/ 2> /dev/null
+
 # Install better discord
 echo "[INFO] Installing BetterDiscord"
 curl -L https://github.com/BetterDiscord/Installer/releases/latest/download/BetterDiscord-Linux.AppImage -o ~/BetterDiscord-Linux.AppImage
 chmod +x ~/BetterDiscord-Linux.AppImage
 ~/BetterDiscord-Linux.AppImage
 while pgrep betterdiscord > /dev/null; do sleep 1; done
-rm ~/BetterDiscord-Linux.AppImage
-
-# Copy config files
-echo "[INFO] Copy config files"
-export XDG_CONFIG_HOME=$HOME/.config
-cp -r ~/.dotfiles/config ~/.config 2> /dev/null
-cp ~/.dotfiles/home/.* ~/ 2> /dev/null
+rm -rf ~/BetterDiscord-Linux.AppImage
 
 # Remove bash files
 echo "[INFO] Removing bash files"
