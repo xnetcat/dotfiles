47a8d14ddc9ed3cdcd8525ae039b53a7aba276f8
Jakub Kot <42355410+xnetcat@users.noreply.github.com>
2021-04-27 14:56:35 +0200

    finished volume notifications

--- config/sxhkd/scripts/volume.sh
+++ config/sxhkd/scripts/volume.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 # You can call this script like this:
@@ -6,44 +5,38 @@
 # $./volume.sh down
 # $./volume.sh mute
 
-function get_volume {
-    amixer get Master | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1 > /dev/null
-}
-
-function is_mute {
-    amixer get Master | grep '%' | grep -oE '[^ ]+$' | grep off > /dev/null
-}
-
 function send_notification {
-	volume=$(get_volume)
+	volume=$(amixer get Master | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1) > /dev/null
+  	bar=$(seq -s "─" 0 $(("$volume" / 3)) | sed 's/[0-9]//g')
+	text="    $bar"
 
-	if (($volume >= 75)); then
+	if amixer get Master | grep '%' | grep -oE '[^ ]+$' | grep off > /dev/null; then
+		icon="audio-volume-muted"
+		text="Muted"
+	elif [[ (("$volume" -gt 74)) ]]; then
 		icon="audio-volume-high"
-	elif (($volume <= 25)); then
+	elif [[ (("$volume" -lt 26)) ]]; then
 		icon="audio-volume-low"
 	else
 		icon="audio-volume-medium"
 	fi
   	
-  	bar=$(seq -s "─" 0 $((volume / 5)) | sed 's/[0-9]//g')
-  	dunstify -i "$icon" -r 5555 -u normal "    $bar"
+  	dunstify -i "$icon" -r 5555 -u normal "$text"
 }
+
+
 
 case $1 in
     up)
-    amixer -D pulse sset Master 5%+ unmute
-	send_notification
+    	amixer -D pulse sset Master 5%+ unmute
+		send_notification
 	;;
     down)
-    amixer -D pulse sset Master 5%- unmute
-	send_notification
+    	amixer -D pulse sset Master 5%- unmute
+		send_notification
 	;;
     mute)
-	amixer -D pulse sset Master toggle-mute
-	if is_mute ; then
-	    dunstify -i "audio-volume-muted" -r 5555 -u normal "Mute"
-	else
-	    send_notification
-	fi
+		amixer -D pulse sset Master toggle-mute
+		send_notification
 	;;
 esac
