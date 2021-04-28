d3024fdea9315293dc13b01c1eb3e19fd90417b7
Jakub Kot <42355410+xnetcat@users.noreply.github.com>
2021-04-28 17:53:37 +0200

    spicetify

--- install.sh
+++ install.sh
@@ -77,6 +77,15 @@ chmod +x ~/BetterDiscord-Linux.AppImage
 while pgrep betterdiscord > /dev/null; do sleep 1; done
 rm -rf ~/BetterDiscord-Linux.AppImage
 
+# Configure spicetify
+sudo chmod a+wr /opt/spotify
+sudo chmod a+wr /opt/spotify/Apps -R
+spicetify
+spicetify backup apply enable-devtool
+spicetify config current_theme Dribbblish color_scheme horizon
+spicetify config inject_css 1 replace_colors 1 overwrite_assets 1
+spicetify apply
+
 # Remove bash files
 echo "[INFO] Removing bash files"
 mv .bash* ~/.backup_dotfiles/ 2> /dev/null
