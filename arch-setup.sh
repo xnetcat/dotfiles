06310aff7b6f131b97def58bfe7b64b43310b89d
Jakub Kot <42355410+xnetcat@users.noreply.github.com>
2021-08-18 21:10:18 +0200

    working on new rice

--- arch-setup.sh
+++ arch-setup.sh
@@ -16,7 +16,7 @@ echo -n "Would you like to wipe and re-partition the disk $device ? (Y/n): "
 read -r wipe
 
 if [ ! "$wipe" = "n" ]; then
-    echo "[INFO]: Wiping the partition table..."
+    echo "Wiping the partition table..."
     wipefs -a -f "$device"
     sleep 1
 fi
@@ -33,7 +33,6 @@ clear
 
 lsblk "$device"
 echo ""
-echo "Now you will specify the partitions you have created"
 
 # Boot partition
 echo -n "Please enter boot partition (example: /dev/sda1): "
@@ -52,7 +51,7 @@ if [ "$swap_s" = "y" ]; then
 fi
 
 # Home partition
-echo -n "Did you create a home partition?(y/N): "
+echo -n "Did you create a home partition? (y/N): "
 read -r home_s
 if [ "$home_s" = "y" ]; then
     echo -n "Please enter home partition (example: /dev/sda4): "
@@ -60,41 +59,41 @@ if [ "$home_s" = "y" ]; then
 fi
 
 # Create root partition
-echo "[INFO] Formatting root partition"
+echo "Formatting root partition"
 mkfs.ext4 "$root"
-echo "[INFO] Mounting root partition"
+echo "Mounting root partition"
 mount "$root" /mnt
 
 # Create the boot partition
 echo -n "Do you want to use efi or legacy bios? (e/L): "
 read -r bios_s
 if [ "$bios_s" = "e" ]; then # efi
-    echo "[INFO] Formatting boot partition"
+    echo "Formatting boot partition"
     mkfs.fat -F32 "$boot"
-    echo "[INFO] Mounting boot partition"
+    echo "Mounting boot partition"
     mkdir /mnt/boot/EFI -p
     mount "$boot" /mnt/boot/EFI
 else # legacy
-    echo "[INFO] Formatting boot partition"
+    echo "Formatting boot partition"
     mkfs.ext4 "$boot"
-    echo "[INFO] Mounting boot partition"
+    echo "Mounting boot partition"
     mkdir /mnt/boot
     mount "$boot" /mnt/boot  
 fi
 
 # Create swap partition
 if [ "$swap_s" = "y" ]; then
-    echo "[INFO] Creating swap partition"
+    echo "Creating swap partition"
     mkswap "$swap"
-    echo "[INFO] Mounting swap partition"
+    echo "Mounting swap partition"
     swapon "$swap"
 fi
 
 # Create home partition
 if [ "$home_s" = "y" ]; then
-    echo "[INFO] Creating home partition"
+    echo "Creating home partition"
     mkfs.ext4 "$home"
-    echo "[INFO] Mounting home partition"
+    echo "Mounting home partition"
     mkdir /mnt/home
     mount "$home" /mnt/home
 fi
@@ -102,21 +101,21 @@ fi
 # Install
 echo "Press ENTER to install required packages"
 read -r _
-echo "[INFO] Installing required packages"
+echo "Installing required packages"
 pacstrap /mnt base base-devel linux linux-firmware git vim
 
 # Generate fstab entries for mounted partitions
-echo "[INFO] Generating fstab entries for mounted partitions"
+echo "Generating fstab entries for mounted partitions"
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Copy chroot script to /mnt
-echo "[INFO] Copying install script"
+echo "Copying install script"
 cp ~/.dotfiles/arch-setup/chroot.sh /mnt/root/chroot-install.sh
 
 # Set correct permissions for chroot scripts
-echo "[INFO] Setting permissions for chroot script"
+echo "Setting permissions for chroot script"
 chmod +x /mnt/root/chroot-install.sh 
 
 # Change root directory to arch
-echo "[INFO] Change arch root directory to /mnt"
+echo "Change arch root directory to /mnt"
 arch-chroot /mnt /root/chroot-install.sh
--- arch-setup/chroot.sh
+++ arch-setup/chroot.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 # Set zoneinfo
-echo -n "Please enter you zoneinfo (example /usr/share/zoneinfo/Europe/Warsaw): "
+echo -n "Please enter you zoneinfo (example: Europe/Warsaw): "
 read -r zoneinfo
-ln -sf "$zoneinfo" /etc/localtime
-
-# Sync hardware clock
-echo "[INFO] Setting hardware clock"
-hwclock --systohc
+ln -sf /usr/share/zoneinfo/$zoneinfo /etc/localtime
 
 echo -n "Please configure you locale, press ENTER to continue"
 read -r _
@@ -20,35 +16,20 @@ read -r _
 
 echo -n "Please enter locale that you specified (example: en_US.UTF-8): "
 read -r  locale
-echo "[INFO] Setting locale"
+echo "Setting locale"
 echo "LANG=$locale" >> /etc/locale.conf
 
 # Generate locales
-echo "[INFO] Generating locales"
+echo "Generating locales"
 locale-gen
-
-# Set keyboard
-echo -n "Do you want to set keyboard layout? (y/N): "
-read -r keyboard_s
-if [ "$keyboard_s" = "y" ]; then
-    echo -n "Do you want to see all possible keyboard layouts? (y/N): "
-    read -r layouts_s
-    if [ "$layouts_s" = "y" ]; then
-        localectl list-keymaps
-    fi
-    echo -n "Please enter you keymap (example: us): "
-    read -r keymap
-    echo "[INFO] Setting new keymap"
-    localectl set-keymap --noconvert "$keymap"
-fi
 
 # Hostname
 echo -n "Do you want to change hostname? (Y/n): "
-read -r keyboard_s
-if [ ! "$keyboard_s" = "n" ]; then
+read -r hostname_s
+if [ ! "$hostname_s" = "n" ]; then
     echo -n "Please enter your hostname: "
     read -r new_hostname
-    echo "[INFO] Setting new hostname"
+    echo "Setting new hostname"
     echo "$new_hostname" > /etc/hostname
 fi
 
@@ -56,16 +37,16 @@ fi
 echo -n "Do you want to change root password? (Y/n): "
 read -r root_s
 if [ ! "$root_s" = "n" ]; then
-    echo "[INFO] Setting root password"
+    echo "Setting root password"
     passwd
 fi
 
 # Install network manager and grub
-echo "[INFO] Installing networkmanager and grub"
+echo "Installing networkmanager and grub"
 pacman -S networkmanager grub
 
 # Start network manager on start
-echo "[INFO] Setting network manager to run on start"
+echo "Setting network manager to run on start"
 systemctl enable NetworkManager
 
 # Add user
@@ -74,37 +55,34 @@ read -r new_username
 useradd -m -G wheel,video,audio,storage "$new_username"
 
 # Set password for user
-echo "[INFO] Setting password for user"
+echo "Setting password for user"
 passwd "$new_username"
 
-# Prevent user from entering sudo passwords for new terminals !! this is not really safe, i am just lazy !!
-echo "[INFO] Prevent user from entering sudo passwords for new terminals"
-echo "$(echo 'Defaults !tty_tickets'; cat /etc/sudoers)" > /etc/sudoers
-
-# Allow users to execute any command
-echo "[INFO] Setting permissions for wheel group"
-echo "$(echo '%wheel ALL=(ALL) ALL'; cat /etc/sudoers)" > /etc/sudoers
+cat > /etc/sudoers << EOF
+%wheel ALL=(ALL) ALL
+root ALL=(ALL) ALL
+Defaults !tty_tickets
+@includedir /etc/sudoers.d
+EOF
 
 # Configure grub
 echo -n "Are you using efi or legacy bios? (e/L): "
 read -r bios_s
-echo "[INFO] Configuring grub"
+echo "Configuring grub"
 if [ "$bios_s" = "e" ]; then # efi bios
     pacman -S efibootmgr
-    echo "[INFO] Installing grub"
+    echo "Installing grub"
     grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=GRUB
-    echo "[INFO] Generating grub config"
+    echo "Generating grub config"
     grub-mkconfig -o /boot/grub/grub.cfg
 else # legacy bios
     echo -n "Please enter the install device for grub (example: /dev/sda): "
     read -r grub_s
-    echo "[INFO] Installing grub"
+    echo "Installing grub"
     grub-install "$grub_s"
-    echo "[INFO] Generating grub config"
+    echo "Generating grub config"
     grub-mkconfig -o /boot/grub/grub.cfg
 fi
 
-# end
-exit
 echo "Installation has finished, you can now unmount and reboot"
-echo "Look if there were any errors, GL&HF"
+echo "Check if there were any errors, GL&HF"
